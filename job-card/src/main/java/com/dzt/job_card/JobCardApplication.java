9717cf165d0d51a9b8fd567d35636b2c57326350
Corey Stamper <cstamper93@gmail.com>
2024-07-23 23:38:47 -0400

    Adding Java file

---
+++ job-card/src/main/java/com/dzt/job_card/JobCardApplication.java
@@ -0,0 +1,13 @@
+package com.dzt.job_card;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobCardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobCardApplication.class, args);
+	}
+
+}
---
+++ job-card/src/test/java/com/dzt/job_card/JobCardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dzt.job_card;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobCardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
