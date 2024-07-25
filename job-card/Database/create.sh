805d516e19c6f9817c5186e36e5de908086c0888
Corey Stamper <cstamper93@gmail.com>
2024-07-25 12:28:04 -0400

    Created shell script to refresh db and sql file to terminate db

---
+++ job-card/Database/create.sh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+export PGPASSWORD='postgres1'
+export PGPORT=15432
+BASEDIR=$(dirname $0)
+DATABASE=dzt_job_card_db
+psql -U postgres -f "$BASEDIR/dropdb.sql" &&
+createdb -U postgres $DATABASE &&
+psql -U postgres -d $DATABASE -f "$BASEDIR/dzt_job_card_script.sql"
