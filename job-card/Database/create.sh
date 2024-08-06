28eea4e60e75a8c36e005da8943e1f225305d5e9
Corey Stamper <cstamper93@gmail.com>
2024-08-06 11:43:27 -0400

    Added test data to sql script

--- job-card/Database/create.sh
+++ job-card/Database/create.sh
@@ -5,4 +5,5 @@ BASEDIR=$(dirname $0)
 DATABASE=dzt_job_card_db
 psql -U postgres -f "$BASEDIR/dropdb.sql" &&
 createdb -U postgres $DATABASE &&
-psql -U postgres -d $DATABASE -f "$BASEDIR/dzt_job_card_script.sql"
+psql -U postgres -d $DATABASE -f "$BASEDIR/dzt_job_card_script.sql" &&
+psql -U postgres -d $DATABASE -f "$BASEDIR/TestData.sql"
