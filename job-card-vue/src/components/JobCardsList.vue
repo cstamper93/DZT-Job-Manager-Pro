faf3c1cbef3e4901ac4d087bd44f2b3333a2b9ed
Corey Stamper <cstamper93@gmail.com>
2024-08-06 10:32:46 -0400

    Created component for job cards display called JobCardsList

--- job-card-vue/src/components/JobCardsList.vue
+++ job-card-vue/src/components/JobCardsList.vue
@@ -1,11 +1,20 @@
 <template>
-    
+    <div>
+        <h1>Jobs Board</h1>
+        <div class="job-card" v-for="jobCard in jobCardsList" v-bind:key="jobCard.id">
+            <p>Job Number: {{ jobCard.jobNumber }}</p>
+            <p>Job Address: {{ jobCard.jobAddress }}</p>
+            <p>Status: {{ jobCard.jobStatus }}</p>
+            <p>Complete By: {{ jobCard.completeBy }}</p>
+        </div>
+    </div>
 </template>
 
 <script>
 import backendService from '../Services/BackendService.js';
 
 export default {
+    name: "job-cards-list",
     data() {
         return {
             jobCards: [],
@@ -24,4 +33,11 @@ export default {
 </script>
 
 <style scoped>
+.job-card {
+    padding: 5px;
+    margin: 5px;
+    border: 1px;
+    width: 250px;
+    background-color: blue;
+}
 </style>
